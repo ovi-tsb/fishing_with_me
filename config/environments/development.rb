04c5c29d0756c10b42daf97afe20466edee8fc81
Ovi Turcu <ovi_tsb@yahoo.com>
2017-08-08 01:00:16 -0400

    production.rb modif

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,12 +57,15 @@ Rails.application.configure do
   Paperclip.options[:command_path] = "/usr/local/bin/"
 
   config.paperclip_defaults = {
-     storage: :s3,
-     s3_credentials: {
-     bucket: ENV.fetch('S3_BUCKET_NAME'),
-     access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
-     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-     s3_region: ENV.fetch('AWS_REGION'),
-     }
+      storage: :s3,
+      s3_host_name: 's3-us-east-2.amazonaws.com',
+      s3_credentials: {
+        bucket: ENV.fetch('S3_BUCKET_NAME'),
+        access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+        secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+        
+      },
+      url:':s3_domain_url',
+      path: '/:class/:attachment/:id_partition/:style/:filename'
     }
 end
