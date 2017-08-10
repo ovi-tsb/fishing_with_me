8c8ba8ad07fec4d85ec0435797235ed243d6ca07
Ovi Turcu <ovi_tsb@yahoo.com>
2017-08-10 02:43:01 -0400

    added user_registration_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,11 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
-  
+  before_action :authenticate_user!,except:[:index, :show]
+ 
   # GET /articles
   # GET /articles.json
+
+
   def index
     @articles = Article.all.order(created_at: :desc).paginate(:page => params[:page], per_page: 7)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+   
+   validates :first_name, presence: true 
+   validates :last_name, presence: true
+
+
    has_many :articles
-   has_many :comments
+   has_many :comments,  dependent: :destroy
    acts_as_voter
 end
